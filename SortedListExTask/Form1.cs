828710b030d137945a3b8649a04e45aa14cb1ac7
ttj66 <ttj66@yahoo.fr>
2019-04-30 12:22:28 -0300

    last update

--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -20,12 +20,60 @@ namespace SortedListExTask
 
         private void btnAddTask_Click(object sender, EventArgs e)
         {
+            try { 
             if ((txtTask.Text).Trim() == String.Empty)
             {
                 MessageBox.Show("You must enter a task");
                 txtTask.Select();
                 txtTask.Focus();
             }
+            else {
+        
+                toDoLitst.Add(dtpTaskDate.Value.Date, txtTask.Text);
+            }
+            DiplayTask();
+            txtTask.Clear();
+            txtTask.SelectAll();
+            txtTask.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Only one task per day is allowed!","Invalid data");
+            }
+
+        }
+        private void DiplayTask()
+        {
+            lstTasks.Items.Clear();
+            foreach(var item in toDoLitst.OrderBy(kvp =>kvp.Key))
+            {
+                lstTasks.Items.Add(item.Key);
+            }
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           lblTaskDetails.Text = toDoLitst[Convert.ToDateTime(lstTasks.SelectedItem)];
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("Do you really want to remove this task?", "Remove task", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (res== DialogResult.Yes) {
+                toDoLitst.Remove(Convert.ToDateTime(lstTasks.SelectedItem));
+                DiplayTask();
+            }
+           
+        }
+
+        private void btnPrintAll_Click(object sender, EventArgs e)
+        {
+            string rpt = "";
+            foreach (var item in toDoLitst)
+            {
+             rpt = rpt +   item.Key.ToShortDateString() + " "+ item.Value + Environment.NewLine;
+            }
+            MessageBox.Show(rpt);
         }
     }
 }
